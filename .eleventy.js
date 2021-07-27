ac77aa5eecf2d0316cc045beeba9427c9af5d032
Kellen Malek <32937586+kjmalek@users.noreply.github.com>
2021-07-26 20:19:24 -0700

    Update to fix CMS

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
     eleventyConfig.addPassthroughCopy("src/media");
+    
+    eleventyConfig.addPassthroughCopy("src/admin/config.yml");
 };
