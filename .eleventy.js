71ec76e06b2c48cb54c73b73c6734a6a602d0afb
Kellen Malek <kellenmalek@hotmail.com>
2021-09-24 17:26:03 -0700

    Update npm scripts & tweak style

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,19 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/favicon.ico");
-
     eleventyConfig.addPassthroughCopy("src/media");
-    
     eleventyConfig.addPassthroughCopy("src/admin/config.yml");
+    eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+        if( outputPath && outputPath.endsWith(".html") ) {
+          let minified = htmlmin.minify(content, {
+            userShortDoctype: true,
+            removeComments: true,
+            collapseWhitespace: true
+          });
+          return minified;
+        }
+    
+        return content;
+      });
 };
